8473bb435198fb5e9b8eee2d594e70249163c249
Rider Adkins <rideradkins@gmail.com>
2015-12-11 22:01:02 -0500

    Renamed generics from bid / ask to price / target currency

--- core/src/main/java/com/fatsoapps/btceparser/core/data/Order.java
+++ core/src/main/java/com/fatsoapps/btceparser/core/data/Order.java
@@ -1,35 +1,30 @@
 package com.fatsoapps.btceparser.core.data;
 
 import com.fatsoapps.btceparser.core.currency.BaseCurrency;
-import com.fatsoapps.btceparser.core.currency.Coin;
-import com.fatsoapps.btceparser.core.currency.Currency;
 import com.fatsoapps.btceparser.core.requests.DepthType;
-
-import java.util.Random;
-import java.util.TreeSet;
 
 public class Order<T extends BaseCurrency<T>, U extends BaseCurrency<U>> implements Comparable<Order<T, U>> {
 
-	private T bidCurrency;
-	private U askCurrency;
+	private T priceCurrency;
+	private U targetCurrency;
 	private DepthType type;
 
 	public Order(T bid, U ask, DepthType type) {
-		bidCurrency = bid;
-		askCurrency = ask;
+		priceCurrency = bid;
+		targetCurrency = ask;
 		this.type = type;
 	}
 
-	public BaseCurrency<T> getBidCurrency() {
-		return bidCurrency;
+	public BaseCurrency<T> getPriceCurrency() {
+		return priceCurrency;
 	}
 
-	public BaseCurrency<U> getAskCurrency() {
-		return askCurrency;
+	public BaseCurrency<U> getTargetCurrency() {
+		return targetCurrency;
 	}
 
 	public BaseCurrency<?> getOrderTotal() {
-		return bidCurrency.multiply(askCurrency);
+		return priceCurrency.multiply(targetCurrency);
 	}
 
 	public DepthType getOrderType(){
@@ -37,20 +32,20 @@ public class Order<T extends BaseCurrency<T>, U extends BaseCurrency<U>> impleme
 	}
 
 	public String toString(){
-		return "[" + type + "] " + bidCurrency.asBigDecimal() + " @ " + askCurrency.asBigDecimal() + " = " + getOrderTotal().asBigDecimal();
+		return "[" + type + "] " + priceCurrency.asBigDecimal() + " @ " + targetCurrency.asBigDecimal() + " = " + getOrderTotal().asBigDecimal();
 	}
 
 	public int compareTo(Order<T, U> other) {
         if (type == DepthType.ASK) {
-            return -askCurrency.asBigDecimal().compareTo(other.getAskCurrency().asBigDecimal());
+            return -targetCurrency.asBigDecimal().compareTo(other.getTargetCurrency().asBigDecimal());
         } else {
-            return bidCurrency.asBigDecimal().compareTo(other.getBidCurrency().asBigDecimal());
+            return priceCurrency.asBigDecimal().compareTo(other.getPriceCurrency().asBigDecimal());
         }
 	}
 
 	public int hashCode() {
 		int hash = (type == DepthType.BID ? 17 : 31);
-		return hash * bidCurrency.hashCode() * askCurrency.hashCode();
+		return hash * priceCurrency.hashCode() * targetCurrency.hashCode();
 	}
 
 
