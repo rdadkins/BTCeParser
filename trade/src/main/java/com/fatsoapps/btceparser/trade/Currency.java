0952b005dd5379195ed3e5bed9f2f8f73c9b1233
Rider Adkins <rideradkins@gmail.com>
2015-12-17 11:26:19 -0500

    Added a Currency enum to map available currencies to interact with in the TAPI

---
+++ trade/src/main/java/com/fatsoapps/btceparser/trade/Currency.java
@@ -0,0 +1,22 @@
+package com.fatsoapps.btceparser.trade;
+
+/**
+ * A list of supported currencies used within the TAPI.
+ */
+public enum Currency {
+
+    BTC,
+    LTC,
+    NMC,
+    NVC,
+    TRC,
+    PPC,
+    FTC,
+    XPM,
+    USD,
+    RUR,
+    EUR,
+    CNH,
+    GBP
+
+}
