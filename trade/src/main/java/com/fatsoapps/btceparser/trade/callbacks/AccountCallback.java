293995f2ed6466c2a36d405dc57245b01f3cec86
Rider Adkins <rideradkins@gmail.com>
2015-12-17 00:34:12 -0500

    Added AccountCallback for all requests made through AccountRequest

---
+++ trade/src/main/java/com/fatsoapps/btceparser/trade/callbacks/AccountCallback.java
@@ -0,0 +1,14 @@
+package com.fatsoapps.btceparser.trade.callbacks;
+
+/**
+ * Simple interface to let the calling class know when something fails. Possible implementations can be used in a GUI
+ * approach where onError lets the user know why something went wrong. onSuccess can let the user know everything is
+ * working properly
+ */
+public interface AccountCallback {
+
+    void onSuccess();
+
+    void onError(String reason);
+
+}
