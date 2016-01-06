0fa129030d5ca52c062fb3ac46fffbd09156e059
Rider Adkins <rideradkins@gmail.com>
2016-01-06 18:54:28 -0500

    Created CancelOrderRequest

---
+++ trade/src/main/java/co/bitsquared/btceparser/trade/requests/CancelOrderRequest.java
@@ -0,0 +1,46 @@
+package co.bitsquared.btceparser.trade.requests;
+
+import co.bitsquared.btceparser.trade.Funds;
+import co.bitsquared.btceparser.trade.ParameterBuilder;
+import co.bitsquared.btceparser.trade.TAPI;
+import co.bitsquared.btceparser.trade.authentication.Authenticator;
+import co.bitsquared.btceparser.trade.callbacks.CancelOrderCallback;
+import co.bitsquared.btceparser.trade.exceptions.MissingParametersException;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONObject;
+
+public class CancelOrderRequest extends AccountRequest {
+
+    private CancelOrderCallback callback;
+
+    public CancelOrderRequest(Authenticator authenticator, long timeout, CancelOrderCallback callback) {
+        super(authenticator, timeout, callback);
+        this.callback = callback;
+    }
+
+    public void processRequest(ParameterBuilder parameters) {
+        if (!parameters.contains("order_id")) {
+            throw new MissingParametersException("order_id");
+        }
+        parameters.method(TAPI.CANCEL_ORDER);
+        super.processRequest(parameters);
+    }
+
+    @Override
+    public void processReturn(JSONObject returnObject) {
+        int orderId = returnObject.getInt("order_id");
+        Funds[] funds = extractFunds(returnObject.getJSONObject("funds"));
+        callback.onSuccess(orderId, funds);
+    }
+
+    @Override
+    public void failed(UnirestException e) {
+        callback.onError(e.getMessage());
+    }
+
+    @Override
+    public void cancelled() {
+        callback.onError("User cancelled request");
+    }
+
+}
