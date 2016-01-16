fa1aae6f63a25464eab7f46a6aa523aca144662d
Rider Adkins <rideradkins@gmail.com>
2016-01-16 14:17:19 -0500

    Removed TradingPair parameter from CoinInfoCallback since it can be accessed via CoinInfo

--- core/src/main/java/co/bitsquared/btceparser/core/callbacks/CoinInfoCallback.java
+++ core/src/main/java/co/bitsquared/btceparser/core/callbacks/CoinInfoCallback.java
@@ -1,10 +1,9 @@
 package co.bitsquared.btceparser.core.callbacks;
 
-import co.bitsquared.btceparser.core.TradingPair;
 import co.bitsquared.btceparser.core.data.CoinInfo;
 
 public interface CoinInfoCallback extends BaseRequestCallback {
 
-    void onSuccess(TradingPair tradingPair, CoinInfo coinInfo);
+    void onSuccess(CoinInfo coinInfo);
 
 }
