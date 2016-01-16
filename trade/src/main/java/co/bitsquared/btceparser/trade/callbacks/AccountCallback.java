08fa0efc020993c8e72b046168f952b49067872f
Rider Adkins <rideradkins@gmail.com>
2016-01-16 12:53:00 -0500

    Added documentation to AccountCallback. Added cancelled() and changed onError to error

--- trade/src/main/java/co/bitsquared/btceparser/trade/callbacks/AccountCallback.java
+++ trade/src/main/java/co/bitsquared/btceparser/trade/callbacks/AccountCallback.java
@@ -2,13 +2,25 @@ package co.bitsquared.btceparser.trade.callbacks;
 
 /**
  * Simple interface to let the calling class know when something fails. Possible implementations can be used in a GUI
- * approach where onError lets the user know why something went wrong. onSuccess can let the user know everything is
+ * approach where error lets the user know why something went wrong. onSuccess can let the user know everything is
  * working properly
  */
 public interface AccountCallback {
 
+    /**
+     * Called when an AccountRequest returns a success == 1.
+     */
     void onSuccess();
 
-    void onError(String reason);
+    /**
+     * Called when there is an error with a reason given.
+     * @param reason the error.
+     */
+    void error(String reason);
+
+    /**
+     * Called when a user cancels a request.
+     */
+    void cancelled();
 
 }
