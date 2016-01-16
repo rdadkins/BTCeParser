344e967fbc49090c663924f051114973fe70c669
Rider Adkins <rideradkins@gmail.com>
2016-01-16 13:18:16 -0500

    Updated the definition of CoinInfo. There are no longer Requests being made from this class.

--- core/src/main/java/co/bitsquared/btceparser/core/data/CoinInfo.java
+++ core/src/main/java/co/bitsquared/btceparser/core/data/CoinInfo.java
@@ -1,26 +1,11 @@
 package co.bitsquared.btceparser.core.data;
 
-import co.bitsquared.btceparser.core.API;
 import co.bitsquared.btceparser.core.TradingPair;
-import co.bitsquared.btceparser.core.callbacks.CoinInfoUpdater;
-import co.bitsquared.btceparser.core.callbacks.RequestCallback;
-import co.bitsquared.btceparser.core.requests.PublicRequest;
-import co.bitsquared.btceparser.core.RequestType;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONObject;
 
-/**
- * CoinInfo gathers the general coin information via GETINFO method that returns information of how to trade a coin / TradingPair.
- * @see co.bitsquared.btceparser.core.TradingPair for auto get requests on static initilization.
- */
-public class CoinInfo implements RequestCallback<JsonNode> {
+public class CoinInfo {
 
     private final TradingPair tradingPair;
-    private CoinInfoUpdater updater;
-    private PublicRequest request;
-    private JSONObject rawData;
 
     private int decimalPlaces;
     private double minPrice;
@@ -29,40 +14,9 @@ public class CoinInfo implements RequestCallback<JsonNode> {
     private boolean hidden;
     private double fee;
 
-    public CoinInfo(TradingPair tradingPair, CoinInfoUpdater updater) {
-        this.tradingPair = tradingPair;
-        this.updater = updater;
-    }
-
     public CoinInfo(TradingPair tradingPair, JSONObject data) {
         this.tradingPair = tradingPair;
-        rawData = data;
-        extractData(rawData);
-    }
-
-    public void processInfo() {
-        if (rawData != null) {
-            return;
-        }
-        if (request == null) {
-            request = new PublicRequest(API.INFO.getUrl(null), 10000, this, RequestType.INFO);
-        }
-        request.processRequest();
-    }
-
-    public void cancelled() {
-        if (updater != null) {
-            updater.cancelled();
-        }
-    }
-
-    public void completed(HttpResponse<JsonNode> response, RequestType source) {
-        JSONObject pairs = response.getBody().getObject().getJSONObject("pairs");
-        rawData = pairs.getJSONObject(tradingPair.toString());
-        extractData(rawData);
-        if (updater != null) {
-            updater.onInfoUpdate(tradingPair, decimalPlaces, minPrice, maxPrice, minAmount, hidden, fee);
-        }
+        extractData(data);
     }
 
     /**
@@ -94,12 +48,22 @@ public class CoinInfo implements RequestCallback<JsonNode> {
     }
 
     /**
+     * Returns the fee associated with this TradingPair
+     */
+    public double getFee() {
+        return fee;
+    }
+
+    /**
      * Determines whether this pair is hidden from the public.
      */
     public boolean isHidden() {
         return hidden;
     }
 
+    /**
+     * Returns the TradingPair associated with this information
+     */
     public TradingPair getTradingPair() {
         return tradingPair;
     }
@@ -111,12 +75,6 @@ public class CoinInfo implements RequestCallback<JsonNode> {
         minAmount = getDouble(object, "min_amount");
         hidden = getInt(object, "hidden") == 1;
         fee = getDouble(object, "fee");
-    }
-
-    public void failed(UnirestException reason) {
-        if (updater != null) {
-            updater.error(reason);
-        }
     }
 
     private int getInt(JSONObject pair, String key) {
