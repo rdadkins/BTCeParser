d2ebd026807d0a67767ae2a805753d0c865867cf
Rider Adkins <rideradkins@gmail.com>
2016-01-21 16:28:09 -0500

    Added documentation to Order and changed variable names.

--- core/src/main/java/co/bitsquared/btceparser/core/data/Order.java
+++ core/src/main/java/co/bitsquared/btceparser/core/data/Order.java
@@ -7,57 +7,80 @@ import java.math.BigDecimal;
 
 public class Order implements Comparable<Order> {
 
-	private BaseCurrency<?> priceCurrency;
-	private BaseCurrency<?> targetCurrency;
+	/**
+	 * The rate of this order, or the price.
+	 */
+	private BaseCurrency<?> rate;
+
+	/**
+	 * The amount of this order.
+	 */
+	private BaseCurrency<?> amount;
+
+	/**
+	 * Whether this is a buy / sell order.
+	 */
 	private DepthType type;
 
-	public Order(BaseCurrency<?> bid, BaseCurrency<?> ask, DepthType type) {
-		priceCurrency = bid;
-		targetCurrency = ask;
+    /**
+     * An Order is made from a currency rate and amount with a certain DepthType.
+     * @param rate the rate of which the amount is being sold for.
+     * @param amount the amount of a currency being sold.
+     * @param type whether this is a BID or ASK.
+     */
+	public Order(BaseCurrency<?> rate, BaseCurrency<?> amount, DepthType type) {
+		this.rate = rate;
+		this.amount = amount;
 		this.type = type;
 	}
 
-	public BaseCurrency<?> getPriceCurrency() {
-		return priceCurrency;
+	public BaseCurrency<?> getRate() {
+		return rate;
 	}
 
-	public BaseCurrency<?> getTargetCurrency() {
-		return targetCurrency;
+	public BaseCurrency<?> getAmount() {
+		return amount;
 	}
 
+	/**
+	 * @return the product of the rate and amount with a 0% fee.
+     */
 	public BaseCurrency<?> getOrderTotal() {
 		return getOrderTotal(0);
 	}
 
+	/**
+	 * @return the product of the rate and amount with a fee applied.
+     */
 	public BaseCurrency<?> getOrderTotal(double fee) {
-		return (BaseCurrency<?>) priceCurrency.multiply(targetCurrency).multiply(BigDecimal.valueOf(1.0 - fee));
+		return (BaseCurrency<?>) rate.multiply(amount).multiply(BigDecimal.valueOf(1.0 - fee));
 	}
 
 	public DepthType getOrderType(){
 		return type;
 	}
 
-	public void setPriceCurrency(BaseCurrency<?> priceCurrency) {
-		if (priceCurrency.getClass().equals(this.priceCurrency.getClass())) {
-			this.priceCurrency = priceCurrency;
+	protected void setRate(BaseCurrency<?> rate) {
+		if (rate.getClass().equals(this.rate.getClass())) {
+			this.rate = rate;
 		}
 	}
 
-	public void setTargetCurrency(BaseCurrency<?> targetCurrency) {
-		if (targetCurrency.getClass().equals(this.targetCurrency.getClass())) {
-			this.targetCurrency = targetCurrency;
+	protected void setAmount(BaseCurrency<?> amount) {
+		if (amount.getClass().equals(this.amount.getClass())) {
+			this.amount = amount;
 		}
 	}
 
-	public String toString(){
-		return "[" + type + "] " + priceCurrency.asBigDecimal() + " @ " + targetCurrency.asBigDecimal() + " = " + getOrderTotal().asBigDecimal();
+	public String toString() {
+		return "[" + type + "] " + amount.asBigDecimal() + " @ " + rate.asBigDecimal() + " = " + getOrderTotal().asBigDecimal();
 	}
 
 	public int compareTo(Order other) {
         if (type == DepthType.ASK) {
-            return -targetCurrency.asBigDecimal().compareTo(other.getTargetCurrency().asBigDecimal());
+            return -amount.asBigDecimal().compareTo(other.getAmount().asBigDecimal());
         } else {
-            return priceCurrency.asBigDecimal().compareTo(other.getPriceCurrency().asBigDecimal());
+            return rate.asBigDecimal().compareTo(other.getRate().asBigDecimal());
         }
 	}
 
@@ -66,18 +89,18 @@ public class Order implements Comparable<Order> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Order order = (Order) o;
-        if (priceCurrency != null ? !priceCurrency.equals(order.priceCurrency) : order.priceCurrency != null)
+        if (rate != null ? !rate.equals(order.rate) : order.rate != null)
             return false;
-        if (targetCurrency != null ? !targetCurrency.equals(order.targetCurrency) : order.targetCurrency != null)
+        if (amount != null ? !amount.equals(order.amount) : order.amount != null)
             return false;
         return type == order.type;
     }
 
     @Override
     public int hashCode() {
-        int result = priceCurrency != null ? priceCurrency.hashCode() : 0;
-        result = 31 * result + (targetCurrency != null ? targetCurrency.hashCode() : 0);
-        result = 31 * result + (type != null ? type.hashCode() : 0);
+        int result = rate != null ? rate.hashCode() : 0;
+        result += 31 * result + (amount != null ? amount.hashCode() : 0);
+        result += 31 * result + (type != null ? type.hashCode() : 0);
         return result;
     }
 
