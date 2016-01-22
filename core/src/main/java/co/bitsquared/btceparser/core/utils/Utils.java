df297472dba7bd8c2db2cf9299d99f7b28253ccc
Rider Adkins <rideradkins@gmail.com>
2016-01-21 19:06:58 -0500

    Added extractOrderBook() to Utils

--- core/src/main/java/co/bitsquared/btceparser/core/utils/Utils.java
+++ core/src/main/java/co/bitsquared/btceparser/core/utils/Utils.java
@@ -1,9 +1,16 @@
 package co.bitsquared.btceparser.core.utils;
 
+import co.bitsquared.btceparser.core.DepthType;
 import co.bitsquared.btceparser.core.TradingPair;
 import co.bitsquared.btceparser.core.data.CoinInfo;
 import co.bitsquared.btceparser.core.data.CoinTicker;
+import co.bitsquared.btceparser.core.data.Order;
+import co.bitsquared.btceparser.core.data.OrderBook;
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class Utils {
 
@@ -32,6 +39,34 @@ public class Utils {
     public static CoinTicker extractCoinTicker(JSONObject body, TradingPair tradingPair) {
         JSONObject coinTicker = body.getJSONObject(tradingPair.toString());
         return new CoinTicker(tradingPair, coinTicker);
+    }
+
+    /**
+     * Extracts an OrderBook from the body of a response.
+     * @param body the body of a public response.
+     * @param tradingPair the trading pair associated with the body.
+     * @return an OrderBook from the body.
+     */
+    public static OrderBook extractOrderBook(JSONObject body, TradingPair tradingPair) {
+        OrderBook orderBook = new OrderBook(tradingPair);
+        JSONObject pairObject = body.getJSONObject(tradingPair.toString());
+        orderBook.addAskBook(getBook(tradingPair, pairObject, "asks"));
+        orderBook.addBidBook(getBook(tradingPair, pairObject, "bids"));
+        return orderBook;
+    }
+
+    private static ArrayList<Order> getBook(TradingPair tradingPair, JSONObject pairObject, String type) {
+        final DepthType depthType = (type.equals("asks") ? DepthType.ASK : DepthType.BID);
+        JSONArray book = pairObject.getJSONArray(type);
+        ArrayList<Order> listOfOrders = new ArrayList<Order>();
+        Order order;
+        JSONArray orderArray;
+        for (int i = 0; i < book.length(); i++) {
+            orderArray = book.getJSONArray(i);
+            order = tradingPair.getOrderTemplate(orderArray.getDouble(0), orderArray.getDouble(1), depthType);
+            listOfOrders.add(order);
+        }
+        return listOfOrders;
     }
 
 }
