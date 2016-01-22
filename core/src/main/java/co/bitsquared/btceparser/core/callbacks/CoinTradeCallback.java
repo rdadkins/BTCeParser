4b79f4766eaaaae1a58a74a3289edfdf7cad75ae
Rider Adkins <rideradkins@gmail.com>
2016-01-22 12:00:31 -0500

    Created CoinTradeCallback which serves the same functionality as CoinDepthCallback

---
+++ core/src/main/java/co/bitsquared/btceparser/core/callbacks/CoinTradeCallback.java
@@ -0,0 +1,27 @@
+package co.bitsquared.btceparser.core.callbacks;
+
+import co.bitsquared.btceparser.core.data.OrderBook;
+
+public interface CoinTradeCallback extends BaseRequestCallback {
+
+    /**
+     * Returns a new OrderBook when a request has completed.
+     */
+    void onSuccess(OrderBook orderBook);
+
+    /**
+     * This can be called when you want a Request to reuse an existing OrderBook.
+     *
+     * If the return value is null, a new book will be returned via onSuccess(OrderBook).
+     * If there is a book supplied, then after a successful parse onSuccess(OrderBook) will not be called. Instead,
+     * orderBookUpdated() will be called.
+     */
+    OrderBook getExistingOrderBook();
+
+    /**
+     * Called only when getExistingOrderBook() is supplied with a non-null OrderBook and the parse
+     * was successful.
+     */
+    void orderBookUpdated();
+
+}
