e3e31dd49683943ba0d8ed2869cd3c2f615bcd96
Rider Adkins <rideradkins@gmail.com>
2016-01-23 14:57:00 -0500

    Changed the signature of CoinTradeCallback to return Trades instead of Orders

--- core/src/main/java/co/bitsquared/btceparser/core/callbacks/CoinTradeCallback.java
+++ core/src/main/java/co/bitsquared/btceparser/core/callbacks/CoinTradeCallback.java
@@ -1,20 +1,25 @@
 package co.bitsquared.btceparser.core.callbacks;
 
 import co.bitsquared.btceparser.core.data.OrderBook;
+import co.bitsquared.btceparser.core.data.Trade;
+
+import java.util.ArrayList;
 
 public interface CoinTradeCallback extends BaseRequestCallback {
 
     /**
-     * Returns a new OrderBook when a request has completed.
+     * Called when CoinTradeRequest has successfully parsed trades. Note that this is called regardless of an OrderBook being
+     * supplied via getExistingOrderBook(). If there is an OrderBook supplied, executedTrades will have already been applied to
+     * the book. If there is no book supplied, executedTrades will still return. Just note that there is no additional work
+     * required if getExistingOrderBook() supplies an OrderBook.
+     * @param executedTrades a list of executed trades.
      */
-    void onSuccess(OrderBook orderBook);
+    void onSuccess(ArrayList<Trade> executedTrades);
 
     /**
-     * This can be called when you want a Request to reuse an existing OrderBook.
-     *
-     * If the return value is null, a new book will be returned via onSuccess(OrderBook).
-     * If there is a book supplied, then after a successful parse onSuccess(OrderBook) will not be called. Instead,
-     * orderBookUpdated() will be called.
+     * This can be called when you want a Request to reuse an existing OrderBook. If there is an OrderBook supplied (i.e.
+     * the return from this method != null) then all Trades that have been parsed will be automatically
+     * applied to the OrderBook.
      */
     OrderBook getExistingOrderBook();
 
