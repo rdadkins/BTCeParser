6421abdddd36cca9265d1af94604ff9180ef7e0c
Rider Adkins <rideradkins@gmail.com>
2016-01-23 14:48:28 -0500

    Created Trade which holds information from parsed trades on the public API

---
+++ core/src/main/java/co/bitsquared/btceparser/core/data/Trade.java
@@ -0,0 +1,91 @@
+package co.bitsquared.btceparser.core.data;
+
+import co.bitsquared.btceparser.core.DepthType;
+import co.bitsquared.btceparser.core.TradingPair;
+import co.bitsquared.btceparser.core.exceptions.NoTradingPairSuppliedException;
+import org.json.JSONObject;
+
+public class Trade {
+
+    public static final String TYPE_KEY = "type";
+    public static final String PRICE_KEY = "price";
+    public static final String AMOUNT_KEY = "amount";
+    public static final String TRANSACTION_ID_KEY = "tid";
+    public static final String TIMESTAMP_KEY = "timestamp";
+    public static final String TRADING_PAIR_KEY = "TRADING_PAIR_KEY";
+
+    private TradingPair tradingPair;
+    private DepthType depthType;
+    private double rate;
+    private double amount;
+    private long transactionID;
+    private long timestamp;
+
+    /**
+     * Create a Trade from a TradingPair and a JSONObject from a Request response.
+     * @param tradingPair the TradingPair that matches to this trade.
+     * @param tradeObject a JSONObject that came from a Request.
+     */
+    public Trade(TradingPair tradingPair, JSONObject tradeObject) {
+        this.tradingPair = tradingPair;
+        extractJSON(tradeObject);
+    }
+
+    /**
+     * Creates a Trade from a saved string (most commonly stored on a file).
+     * @param tradeAsString a string that has been stored from calling asJSONString().
+     * @throws org.json.JSONException if tradeAsString is not formatted properly.
+     * @throws NoTradingPairSuppliedException if tradeAsString does NOT contain a proper TradingPair.
+     * @return a Trade object from the string provided.
+     */
+    public static Trade fromSavedFile(String tradeAsString) {
+        JSONObject tradeObject = new JSONObject(tradeAsString);
+        String tradingPairAsString = tradeObject.getString(TRADING_PAIR_KEY);
+        if (tradingPairAsString != null) {
+            return new Trade(TradingPair.valueOf(tradingPairAsString), tradeObject);
+        }
+        throw new NoTradingPairSuppliedException(tradeAsString);
+    }
+
+    public TradingPair getTradingPair() {
+        return tradingPair;
+    }
+
+    public DepthType getDepthType() {
+        return depthType;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public long getTransactionID() {
+        return transactionID;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String asJSONString() {
+        return new JSONObject().append(TRADING_PAIR_KEY, tradingPair.name()).
+                append(TYPE_KEY, depthType.toString()).
+                append(PRICE_KEY, rate).
+                append(AMOUNT_KEY, amount).
+                append(TRANSACTION_ID_KEY, transactionID).
+                append(TIMESTAMP_KEY, timestamp).toString();
+    }
+
+    private void extractJSON(JSONObject tradeObject) {
+        depthType = tradeObject.getString(TYPE_KEY).equals("ask") ? DepthType.ASK : DepthType.BID;
+        rate = tradeObject.getDouble(PRICE_KEY);
+        amount = tradeObject.getDouble(AMOUNT_KEY);
+        transactionID = tradeObject.getLong(TRANSACTION_ID_KEY);
+        timestamp = tradeObject.getLong(TIMESTAMP_KEY);
+    }
+
+}
