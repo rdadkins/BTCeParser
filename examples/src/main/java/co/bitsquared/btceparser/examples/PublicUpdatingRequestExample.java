acdc2382ae46d552039ecf232ab5caaa0985184b
Rider Adkins <rideradkins@gmail.com>
2016-01-23 23:13:53 -0500

    Added an example on how to use PublicUpdatingRequest

---
+++ examples/src/main/java/co/bitsquared/btceparser/examples/PublicUpdatingRequestExample.java
@@ -0,0 +1,38 @@
+package co.bitsquared.btceparser.examples;
+
+import co.bitsquared.btceparser.core.TradingPair;
+import co.bitsquared.btceparser.core.callbacks.CoinTickerCallback;
+import co.bitsquared.btceparser.core.data.CoinTicker;
+import co.bitsquared.btceparser.core.requests.CoinTickerRequest;
+import co.bitsquared.btceparser.core.requests.PublicUpdatingRequest;
+
+public class PublicUpdatingRequestExample implements BaseExample, CoinTickerCallback {
+
+    public static void main(String[] args) {
+        new PublicUpdatingRequestExample().startExample();
+    }
+
+    public void startExample() {
+        CoinTickerRequest request = new CoinTickerRequest(TradingPair.BTC_USD, this);
+        PublicUpdatingRequest publicUpdatingRequest = request.asUpdatingRequest();
+        publicUpdatingRequest.updateInterval(3, false);
+        publicUpdatingRequest.processRequest();
+    }
+
+    public void onSuccess(CoinTicker ticker) {
+        System.out.println(ticker.toString());
+    }
+
+    public void cancelled() {
+        System.out.println("Cancelled request by user.");
+    }
+
+    public void error(String reason) {
+        System.out.println("Error: " + reason);
+    }
+
+    public void onSuccess() {
+        System.out.println("Successful response.");
+    }
+
+}
