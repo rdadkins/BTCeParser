31d0c5a84e0b698866986648e9b69e32f608e2be
Rider Adkins <rideradkins@gmail.com>
2016-01-23 21:30:57 -0500

    Removed an unneeded StringBuilder from SHA256

--- core/src/main/java/co/bitsquared/btceparser/core/hash/SHA256.java
+++ core/src/main/java/co/bitsquared/btceparser/core/hash/SHA256.java
@@ -15,10 +15,6 @@ public class SHA256 {
         assert md != null;
         md.update(input.getBytes());
         byte byteData[] = md.digest();
-        StringBuilder sb = new StringBuilder();
-        for (byte aByteData: byteData) {
-            sb.append(Integer.toString((aByteData & 0xff) + 0x100, 16).substring(1));
-        }
         StringBuilder hexString = new StringBuilder();
         for (byte aByteData: byteData) {
             String hex = Integer.toHexString(0xff & aByteData);
