07788b063053338fa9f791c4c3ab58679c164347
Rider Adkins <rideradkins@gmail.com>
2016-01-31 16:44:59 -0500

    Created OrderBookExample

---
+++ examples/src/main/java/co/bitsquared/btceparser/examples/OrderBookExample.java
@@ -0,0 +1,74 @@
+package co.bitsquared.btceparser.examples;
+
+import co.bitsquared.btceparser.core.TradingPair;
+import co.bitsquared.btceparser.core.callbacks.CoinDepthCallback;
+import co.bitsquared.btceparser.core.callbacks.CoinTradeCallback;
+import co.bitsquared.btceparser.core.data.OrderBook;
+import co.bitsquared.btceparser.core.data.Trade;
+import co.bitsquared.btceparser.core.requests.CoinDepthRequest;
+import co.bitsquared.btceparser.core.requests.CoinTradeRequest;
+import co.bitsquared.btceparser.core.requests.PublicUpdatingRequest;
+
+import java.util.ArrayList;
+
+public class OrderBookExample implements BaseExample, CoinTradeCallback, CoinDepthCallback {
+
+    private TradingPair tradingPair = TradingPair.BTC_USD;
+    private OrderBook myOrderBook;
+
+    public static void main(String[] args) {
+        new OrderBookExample().startExample();
+    }
+
+    public void startExample() {
+        startUpdatingTradeRequest();
+        startUpdatingDepthRequest();
+    }
+
+    private void startUpdatingTradeRequest() {
+        CoinTradeRequest tradeRequest = new CoinTradeRequest(tradingPair, 100, this);
+        PublicUpdatingRequest publicUpdatingRequest = tradeRequest.asUpdatingRequest();
+        publicUpdatingRequest.updateInterval(7, false);
+        publicUpdatingRequest.processRequest();
+    }
+
+    private void startUpdatingDepthRequest() {
+        CoinDepthRequest depthRequest = new CoinDepthRequest(tradingPair, 100, this);
+        PublicUpdatingRequest depthUpdatingRequest = depthRequest.asUpdatingRequest();
+        depthUpdatingRequest.updateInterval(13, false);
+        depthUpdatingRequest.processRequest();
+    }
+
+    public void onSuccess(ArrayList<Trade> executedTrades) {
+        System.out.println("Total new trades: " + executedTrades.size());
+    }
+
+    public void onSuccess(OrderBook orderBook) {
+        System.out.println("Separate OrderBook supplied.");
+        myOrderBook = orderBook;
+    }
+
+    public OrderBook getExistingOrderBook() {
+        return myOrderBook;
+    }
+
+    public void orderBookUpdated() {
+        System.out.println("OrderBook has been updated.");
+        System.out.println("\tBid Volume: " + myOrderBook.getTotalBidVolume().asBigDecimal() +
+                "\n\tBid Depth: " + myOrderBook.getTotalBidDepth(0.0).asBigDecimal());
+        System.out.println("\tAsk Volume: " + myOrderBook.getTotalAskVolume().asBigDecimal() +
+                "\n\tAsk Depth: " + myOrderBook.getTotalAskDepth(0.0).asBigDecimal());
+    }
+
+    public void cancelled() {
+        System.out.println("User cancelled this request.");
+    }
+
+    public void error(String reason) {
+        System.out.println("Error: " + reason);
+    }
+
+    public void onSuccess() {
+    }
+
+}
