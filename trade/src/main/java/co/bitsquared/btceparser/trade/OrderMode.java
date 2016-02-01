eda93a493ffc00084da1ea62d3219c4b383838ac
Rider Adkins <rideradkins@gmail.com>
2016-02-01 09:09:57 -0500

    Added documentation to OrderMode and added a constructor

--- trade/src/main/java/co/bitsquared/btceparser/trade/OrderMode.java
+++ trade/src/main/java/co/bitsquared/btceparser/trade/OrderMode.java
@@ -1,8 +1,34 @@
 package co.bitsquared.btceparser.trade;
 
+/**
+ * OrderMode is a sorting mode used in ParameterBuilder when viewing TradeHistory or TransHistory. The default value is
+ * DESC (descending mode).
+ * @see co.bitsquared.btceparser.trade.ParameterBuilder
+ * @see co.bitsquared.btceparser.trade.requests.TradeHistoryRequest
+ * @see co.bitsquared.btceparser.trade.requests.TransHistoryRequest
+ */
 public enum OrderMode {
 
-    ASC,
-    DESC
+    /**
+     * Ascending mode in sorting [smallest to largest].
+     */
+    ASC("ASC"),
+
+    /**
+     * Descending mode in sorting [largest to smallest].
+     */
+    DESC("DESC");
+
+    public static final OrderMode DEFAULT_ORDER_MODE = DESC;
+
+    private String modeAsString;
+
+    OrderMode(String modeAsString) {
+        this.modeAsString = modeAsString;
+    }
+
+    public String getModeAsString() {
+        return modeAsString;
+    }
 
 }
