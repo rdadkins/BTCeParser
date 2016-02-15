725c6f0e32b875ab223670706d734a5373953f82
Rider Adkins <rideradkins@gmail.com>
2016-02-15 13:53:02 -0500

    Added data structures to bot.

---
+++ bot/src/main/java/co/bitsquared/btceparser/bot/data/PriceComparison.java
@@ -0,0 +1,15 @@
+package co.bitsquared.btceparser.bot.data;
+
+public enum PriceComparison {
+
+    /**
+     * Comparison is based on values being greater than OR equal to some value.
+     */
+    GREATER_THAN,
+
+    /**
+     * Comparison is based on values being less than OR equal to.
+     */
+    LESS_THAN
+
+}
