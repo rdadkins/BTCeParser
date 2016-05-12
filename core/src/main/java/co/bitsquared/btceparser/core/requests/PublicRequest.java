82fc051f2b5b8540ae6f8c992ac33b3e8b641d1c
Rider Adkins <rideradkins@gmail.com>
2016-05-12 14:22:08 -0400

    General documentation to PublicRequest

--- core/src/main/java/co/bitsquared/btceparser/core/requests/PublicRequest.java
+++ core/src/main/java/co/bitsquared/btceparser/core/requests/PublicRequest.java
@@ -6,6 +6,14 @@ import com.mashape.unirest.http.Unirest;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * PublicRequest is the basis for requests over the public API.
+ *
+ * @see co.bitsquared.btceparser.core.requests.CoinDepthRequest
+ * @see co.bitsquared.btceparser.core.requests.CoinInfoRequest
+ * @see co.bitsquared.btceparser.core.requests.CoinTickerRequest
+ * @see co.bitsquared.btceparser.core.requests.CoinTradeRequest
+ */
 public abstract class PublicRequest extends Request {
 
     protected static final HashMap<String, Object> DEFAULT_PARAMETERS = new HashMap<String, Object>();
@@ -23,6 +31,9 @@ public abstract class PublicRequest extends Request {
         task = Unirest.get(url).queryString(getHeaders()).asJsonAsync(this);
 	}
 
+    /**
+     * Returns optional headers to pass for this request. If there are no headers, {@code DEFAULT_PARAMETERS} should be returned.
+     */
     protected abstract Map<String, Object> getHeaders();
 
     public abstract PublicUpdatingRequest asUpdatingRequest();
