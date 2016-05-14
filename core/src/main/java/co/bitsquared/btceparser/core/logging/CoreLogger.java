b35d5b1e2580eeeb011b8047e801f4632f9af9de
Rider Adkins <rideradkins@gmail.com>
2016-05-14 16:39:44 -0400

    Used a static init block to create an instance of CoreLogger

--- core/src/main/java/co/bitsquared/btceparser/core/logging/CoreLogger.java
+++ core/src/main/java/co/bitsquared/btceparser/core/logging/CoreLogger.java
@@ -4,24 +4,25 @@ import java.io.IOException;
 
 public class CoreLogger extends AbstractLogger {
 
-    private static CoreLogger ourInstance;
+    private static CoreLogger instance;
     private static final String SECONDARY_PATH = "core";
     private static final String FILE_NAME = "log.txt";
     private static boolean includeDate = false;
+
+    static {
+        try {
+            instance = new CoreLogger();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private CoreLogger() throws IOException {
         super(FILE_NAME, includeDate);
     }
 
     public static CoreLogger getInstance() {
-        if (ourInstance == null) {
-            try {
-                ourInstance = new CoreLogger();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return ourInstance;
+        return instance;
     }
 
     @Override
