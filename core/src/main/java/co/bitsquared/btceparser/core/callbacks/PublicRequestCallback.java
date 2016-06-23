ff9fd2b1feb31d4e6a3041a0f6367e8a7b593a7b
Rider Adkins <rideradkins@gmail.com>
2016-06-23 19:37:00 -0400

    Created a convenience Callback class for public requests

---
+++ core/src/main/java/co/bitsquared/btceparser/core/callbacks/PublicRequestCallback.java
@@ -0,0 +1,61 @@
+package co.bitsquared.btceparser.core.callbacks;
+
+import co.bitsquared.btceparser.core.data.CoinInfo;
+import co.bitsquared.btceparser.core.data.CoinTicker;
+import co.bitsquared.btceparser.core.data.OrderBook;
+import co.bitsquared.btceparser.core.data.Trade;
+
+import java.util.ArrayList;
+
+/**
+ * PublicRequestCallback is a collection of all callbacks with the core module that relate to public requests.
+ * Implementation follows from overriding the methods you need.
+ */
+public class PublicRequestCallback implements CoinDepthCallback, CoinInfoCallback, CoinTickerCallback, CoinTradeCallback {
+
+    @Override
+    public void onSuccess(OrderBook orderBook) {
+
+    }
+
+    @Override
+    public void onSuccess(ArrayList<Trade> executedTrades) {
+
+    }
+
+    @Override
+    public OrderBook getExistingOrderBook() {
+        return null;
+    }
+
+    @Override
+    public void orderBookUpdated() {
+
+    }
+
+    @Override
+    public void onSuccess(CoinInfo coinInfo) {
+
+    }
+
+    @Override
+    public void onSuccess(CoinTicker ticker) {
+
+    }
+
+    @Override
+    public void cancelled() {
+
+    }
+
+    @Override
+    public void error(String reason) {
+
+    }
+
+    @Override
+    public void onSuccess() {
+
+    }
+
+}
