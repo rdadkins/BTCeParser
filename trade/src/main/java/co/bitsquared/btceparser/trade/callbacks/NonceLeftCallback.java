c0c77c71ae008db5851d30372995790578e8f6e7
Rider Adkins <rideradkins@gmail.com>
2016-06-22 23:52:03 -0400

    Updated NonceLeftCallback to meet Oracle documentation standards

--- trade/src/main/java/co/bitsquared/btceparser/trade/callbacks/NonceLeftCallback.java
+++ trade/src/main/java/co/bitsquared/btceparser/trade/callbacks/NonceLeftCallback.java
@@ -4,8 +4,8 @@ public interface NonceLeftCallback {
 
     /**
      * Called whenever getNonce() is called in Authenticator. Implementation needs to follow one of two guidelines:
-     * <br/>&nbsp;1. There is not a lot of logic handled with this return statement.
-     * <br/>&nbsp;2. Implementation must be in a new thread (i.e. run everything in a separate thread when this is called)
+     * <p>1. There is not a lot of logic handled with this return statement.
+     * <p>2. Implementation must be in a new thread (i.e. run everything in a separate thread when this is called)
      * Reason being is that when getNonce() is being used to create a request, it must wait until the implementation is done.
      * So for efficiency, one of the guidelines should be followed.
      * @param amount the amount of times this nonce can be used.
